6b391704f7b02955249cb311ee88a639620f9f5a
Yanfeng Hao <jasonhao518@gmail.com>
2023-01-30 21:14:46 +0100

    use init script update matomo salt

--- ghost/patch/custom_init_script.sql
+++ ghost/patch/custom_init_script.sql
@@ -1,2 +1,2 @@
 use ghost;
-INSERT INTO `matomo_user_token_auth` (`idusertokenauth`,`login`,`description`,`password`,`hash_algo`,`system_token`,`last_used`,`date_created`,`date_expired`) VALUES (2,'demo','tracking','17b08e6bcb9d68f3e3e76ef4eace49c689ec13b50b82ec77d8507d380543e56ade2430e269950e03a0009bb814f3a3c104d982036f9765faad776a67b6dedfec','sha512',0,NULL,'2023-01-30 19:37:25',NULL);
+INSERT INTO `matomo_user_token_auth` (`idusertokenauth`,`login`,`description`,`password`,`hash_algo`,`system_token`,`last_used`,`date_created`,`date_expired`) VALUES (2,'demo','tracking','c66deb51146b875f7afa35f28f61166d27cb223bf9c7ed7e753e2545f8a0cb9b0a8bb8a80b3c6488e0836355b0d6ea46da1176bd08c49743d8137d10ce43dbc4','sha512',0,NULL,'2023-01-30 19:37:25',NULL);
